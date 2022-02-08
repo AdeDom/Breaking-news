e38b4eb1febd757cd816d72996b6984c5ff2ecc5
pathiphon <pathiphon.ja@chococrm.com>
2022-02-08 17:06:32 +0700

    Test room database flow

--- app/src/test/java/com/adedom/breakingnews/data/db/dao/GeneralDaoTest.kt
+++ app/src/test/java/com/adedom/breakingnews/data/db/dao/GeneralDaoTest.kt
@@ -2,16 +2,20 @@ package com.adedom.breakingnews.data.db.dao
 
 import android.content.Context
 import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.asLiveData
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.adedom.breakingnews.data.db.AppDatabase
 import com.adedom.breakingnews.data.db.entities.ArticleDb
 import com.adedom.breakingnews.data.db.entities.GeneralEntity
+import com.adedom.breakingnews.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.stopKoin
@@ -21,13 +25,18 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.P])
 class GeneralDaoTest {
 
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var db: AppDatabase
     private lateinit var dao: GeneralDao
 
     @Before
     fun setUp() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
         dao = db.getGeneralDao()
     }
 
@@ -65,6 +74,36 @@ class GeneralDaoTest {
         val result = dao.getGeneralList()
 
         assertThat(result).isEqualTo(listOf(generalEntity1))
+    }
+
+    @Test
+    fun getGeneralFlow() = runBlocking {
+        val generalEntity = GeneralEntity(
+            2,
+            listOf(
+                ArticleDb(
+                    id = 123456,
+                    author = "Thairath",
+                    title = "\"ศิริราช\" แจง บุคลากร 3 ราย ชาแขน ขา ใบหน้าด้านซ้าย หลังฉีดวัคซีนโควิด - ไทยรัฐ",
+                    description = "รพ.ศิริราช ชี้แจงกรณีบุคลากรของคณะแพทยศาสตร์ 3 ราย ฉีดวัคซีนโควิด-19 แล้วเกิดผลข้างเคียง มีอาการชาบริเวณแขน ขา ใบหน้าด้านซ้าย",
+                    urlToImage = "https://static.thairath.co.th/media/dFQROr7oWzulq5Fa4VuAeHo1zLbnAcXrvxHLvH9G5Rz8Heao69vWyGq85D5arN2qykr.jpg",
+                    publishedAt = "2021-04-23T13:17:12Z",
+                ),
+                ArticleDb(
+                    id = 234567,
+                    author = "Google News",
+                    title = "ไฟไหม้ รพ.อินเดีย ซ้ำเติมวิกฤตโควิด เสียชีวิต 13 คน - Thai PBS News",
+                    description = null,
+                    urlToImage = null,
+                    publishedAt = "2021-04-23T12:59:08Z",
+                )
+            )
+        )
+        dao.saveGeneral(generalEntity)
+
+        val result = dao.getGeneralFlow().asLiveData().getOrAwaitValue()
+
+        assertThat(result).isEqualTo(listOf(generalEntity))
     }
 
     @Test
